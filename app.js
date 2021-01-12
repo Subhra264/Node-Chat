aaf0f4302ce0bf5a2b1ae94458db8e403bf8ed0f
Subhra264 <chakrabortysubhradeep556@gmail.com>
2021-01-12 17:04:31 +0530

    removed socket event listening from app.js

--- app.js
+++ app.js
@@ -34,83 +34,9 @@ mongoose.connection.on("error", (err) => {
 //listen for connections
 io.on("connection" , (socket) => {
     console.log("A user connected!!!");
-    // console.log(socket);
-    // socket.on("join" , (name) => {
-    //     users[socket.id] = name;
-    //     io.emit('usersAvailable', users);
-    //     socket.broadcast.emit("userJoined" , name);
-    //     console.log(users);
-    // });
-    // socket.on("sendMessage" , (message) => {
-    //     socket.broadcast.emit("receiveMessage" , 
-    //                         {name : users[socket.id] ,message});
-        
-    // });
-
-    // //on sending a file
-    // socket.on("fileSent", (file, filetype, fileName) => {
-    //     socket.broadcast.emit("receivedFile", {file, filetype, fileName, name: users[socket.id]});
-    // }); 
-
-    // //On sending an image
-    // socket.on("sendImage", (data) => {
-    //     // console.log("The image data : " + data.data);
-    //     socket.broadcast.emit("receivedImage", {name: users[socket.id], data: data.data });
-    // });
-
-    // //On sending a video
-    // socket.on("sentVideo", (data) => {
-    //     // console.log("The video: " + data);
-    //     socket.broadcast.emit("receivedVideo", {name: users[socket.id], data: data});
-    // });
-
-    // //on sending a voice message
-    // socket.on("sentVoiceMessage" , (chunks) => {
-    //     // console.log("Voice message chunks: " + chunks);
-    //     socket.broadcast.emit("receivedVoice" , chunks , users[socket.id]);
-    // });
-
-    // socket.on("disconnect" , () => {
-    //     socket.broadcast.emit("userLeft" , users[socket.id]);
-    //     delete users[socket.id];
-    //     console.log(socket.id + " left");
-    //     // console.log(users);
-    // });
+    
 });
 
-const data = {
-    user: {
-        name: "John Doe",
-        _id: "5ff15d79e616970f4ce031fb"
-    },
-    groups: [
-        "MyFirstGroup",
-        "MySecondGroup",
-        "MyThirdGroup"
-    ],
-    friends: [],
-    textChannels: [
-        "welcome"
-    ],
-    currentGroup: "MyThirdGroup",
-    users: [
-        {
-            _id: "5ff15dd3e616970f4ce0320b",
-            name: "John Doe",
-            reference: "5ff15d79e616970f4ce031fb"
-        }
-    ],
-    messages: [
-        {
-            sender: {
-                name: "John Doe",
-                reference: "5ff15d79e616970f4ce031fb"
-            },
-            _id: "5ff168dfe0ff013230061c43",
-            message: "Hello world!"
-        }
-    ]
-}
 
 app.get('/', (req , res) => {
     // res.render('chat', {data});
