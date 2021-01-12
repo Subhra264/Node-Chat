27517e29845e90b43b5faab73e5104a99341d8c8
Subhra264 <chakrabortysubhradeep556@gmail.com>
2021-01-12 16:58:56 +0530

    rendered home page

--- app.js
+++ app.js
@@ -29,53 +29,53 @@ mongoose.connection.on("error", (err) => {
 });
 
 //contains the users information
-const users = {};
+// const users = {};
 
 //listen for connections
 io.on("connection" , (socket) => {
     console.log("A user connected!!!");
-    console.log(socket);
-    socket.on("join" , (name) => {
-        users[socket.id] = name;
-        io.emit('usersAvailable', users);
-        socket.broadcast.emit("userJoined" , name);
-        console.log(users);
-    });
-    socket.on("sendMessage" , (message) => {
-        socket.broadcast.emit("receiveMessage" , 
-                            {name : users[socket.id] ,message});
+    // console.log(socket);
+    // socket.on("join" , (name) => {
+    //     users[socket.id] = name;
+    //     io.emit('usersAvailable', users);
+    //     socket.broadcast.emit("userJoined" , name);
+    //     console.log(users);
+    // });
+    // socket.on("sendMessage" , (message) => {
+    //     socket.broadcast.emit("receiveMessage" , 
+    //                         {name : users[socket.id] ,message});
         
-    });
+    // });
 
-    //on sending a file
-    socket.on("fileSent", (file, filetype, fileName) => {
-        socket.broadcast.emit("receivedFile", {file, filetype, fileName, name: users[socket.id]});
-    }); 
+    // //on sending a file
+    // socket.on("fileSent", (file, filetype, fileName) => {
+    //     socket.broadcast.emit("receivedFile", {file, filetype, fileName, name: users[socket.id]});
+    // }); 
 
-    //On sending an image
-    socket.on("sendImage", (data) => {
-        // console.log("The image data : " + data.data);
-        socket.broadcast.emit("receivedImage", {name: users[socket.id], data: data.data });
-    });
+    // //On sending an image
+    // socket.on("sendImage", (data) => {
+    //     // console.log("The image data : " + data.data);
+    //     socket.broadcast.emit("receivedImage", {name: users[socket.id], data: data.data });
+    // });
 
-    //On sending a video
-    socket.on("sentVideo", (data) => {
-        // console.log("The video: " + data);
-        socket.broadcast.emit("receivedVideo", {name: users[socket.id], data: data});
-    });
+    // //On sending a video
+    // socket.on("sentVideo", (data) => {
+    //     // console.log("The video: " + data);
+    //     socket.broadcast.emit("receivedVideo", {name: users[socket.id], data: data});
+    // });
 
-    //on sending a voice message
-    socket.on("sentVoiceMessage" , (chunks) => {
-        // console.log("Voice message chunks: " + chunks);
-        socket.broadcast.emit("receivedVoice" , chunks , users[socket.id]);
-    });
+    // //on sending a voice message
+    // socket.on("sentVoiceMessage" , (chunks) => {
+    //     // console.log("Voice message chunks: " + chunks);
+    //     socket.broadcast.emit("receivedVoice" , chunks , users[socket.id]);
+    // });
 
-    socket.on("disconnect" , () => {
-        socket.broadcast.emit("userLeft" , users[socket.id]);
-        delete users[socket.id];
-        console.log(socket.id + " left");
-        // console.log(users);
-    });
+    // socket.on("disconnect" , () => {
+    //     socket.broadcast.emit("userLeft" , users[socket.id]);
+    //     delete users[socket.id];
+    //     console.log(socket.id + " left");
+    //     // console.log(users);
+    // });
 });
 
 const data = {
@@ -113,7 +113,8 @@ const data = {
 }
 
 app.get('/', (req , res) => {
-    res.render('chat', {data});
+    // res.render('chat', {data});
+    res.render('home');
 });
 
 server.listen(PORT , () => {
