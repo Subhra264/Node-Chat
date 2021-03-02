ac4154c7d49770b171aea66e2f969afd9c2c00bb
Subhra264 <chakrabortysubhradeep556@gmail.com>
2021-03-02 22:07:23 +0530

    Add error.ejs, user.ejs and add controller for profile/:userId route

--- routes/user.route.js
+++ routes/user.route.js
@@ -7,10 +7,28 @@ router.get('/help', auth, (req, res) => {
     res.render('help');
 });
 
-router.get('profile/:username', auth, (req, res) => {
-    const {username} = req.params;
+router.get('profile/:userId', auth, (req, res) => {
+    const { userId } = req.params;
     
-    
+    User.findById( userId, (err, user) => {
+        if(err){
+            return res.status(422).render('error', {
+                status: 422,
+                message: "Oops something went wrong!"
+            });
+        }
+
+        if(!user) {
+            return res.status(404).render('error', {
+                status: 404,
+                message: "Not Found!"
+            });
+        }
+
+        res.render('user', {
+            name: user.name
+        });
+    });
 });
 
 module.exports = router;
