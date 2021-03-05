cf43844170913ca38efca9ddb4d89167d9d302a9
Subhra264 <chakrabortysubhradeep556@gmail.com>
2021-03-05 20:39:31 +0530

    Add user.controller.js and update the title of user.ejs

---
+++ controllers/user.controller.js
@@ -0,0 +1,26 @@
+const User = require('../models/UserModel');
+
+module.exports = {
+    getProfile: (req, res) => {
+        const { userId } = req.params;
+        User.findById( userId, (err, user) => {
+            if(err){
+                return res.status(422).render('error', {
+                    status: 422,
+                    message: "Oops something went wrong!"
+                });
+            }
+    
+            if(!user) {
+                return res.status(404).render('error', {
+                    status: 404,
+                    message: "Not Found!"
+                });
+            }
+    
+            res.render('user', {
+                name: user.name
+            });
+        });
+    }
+}
